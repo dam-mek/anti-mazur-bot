c302119cae6a7f40b718e5a7e27fead52fd66d01
Artemiy Denisov <dammek42@gmail.com>
2020-08-01 05:42:17 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,37 @@
+import config
+import telebot
+import texts
+
+
+bot = telebot.TeleBot(config.TOKEN)
+
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    print(message.chat.username)
+    bot.send_message(message.chat.id, 'I did start')
+    bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAM3Xx3eHjxLZMGi9GQCWRozmRovnAsAAh4DAAKNSjADcnw1sWQ7ES8aBA')
+
+
+@bot.message_handler(content_types=['text'])
+def dialogue(message):
+    print(message.chat.username, message.text)
+    if message.text.lower() in {'suck', 'пососи'}:
+        bot.send_message(message.chat.id, texts.suck)
+    elif message.text.lower() in {'кадиллак', 'кадилак', 'cadillac', 'cadilac'}:
+        bot.send_message(message.chat.id, texts.cadillac)
+    elif message.text.lower() in {'baby', 'малышка'}:
+        bot.send_message(message.chat.id, texts.baby)
+    elif message.text.lower().replace('ё', 'е') in {'ice', 'лед'}:
+        bot.send_message(message.chat.id, texts.ice)
+    else:
+        bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAM3Xx3eHjxLZMGi9GQCWRozmRovnAsAAh4DAAKNSjADcnw1sWQ7ES8aBA')
+    print(message)
+
+
+@bot.message_handler(content_types=['sticker'])
+def start_message(message):
+    print(message)
+
+
+bot.polling()
